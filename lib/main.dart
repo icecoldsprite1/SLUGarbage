7c896b4557dddfb18060274a696c82bb994cbff7
Jeffrey <67610746+icecoldsprite1@users.noreply.github.com>
2024-02-08 03:59:51 -0800

    enter name homepage

--- lib/main.dart
+++ lib/main.dart
@@ -12,17 +12,59 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'SLUGarbage',
       theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange)),
-      home: MyHomePage(),
+          appBarTheme: AppBarTheme(backgroundColor: Colors.yellow.shade600),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.orangeAccent)),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text('SLUGarbage')),
+      body: const TextInputWidget(),
+    );
+  }
+}
+
+class TextInputWidget extends StatefulWidget {
+  const TextInputWidget({super.key});
+
+  @override
+  State<TextInputWidget> createState() => _TextInputWidgetState();
+}
+
+class _TextInputWidgetState extends State<TextInputWidget> {
+  final textController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    textController.dispose();
+  }
+
+  void click() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+          prefixIcon: const Icon(Icons.person),
+          labelText: "Enter a Name",
+          suffixIcon: IconButton(
+            icon: const Icon(Icons.send),
+            tooltip: "Submit",
+            onPressed: click,
+          )),
+    );
   }
 }
