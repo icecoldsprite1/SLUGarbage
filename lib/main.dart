8623710da1e2beb57913a4774af27679ec22aa13
Jeffrey <67610746+icecoldsprite1@users.noreply.github.com>
2024-02-09 03:28:47 -0800

    testing provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:slugarbage/text_input_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,62 +11,47 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'SLUGarbage',
-      theme: ThemeData(
-          appBarTheme: AppBarTheme(backgroundColor: Colors.yellow.shade600),
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.orangeAccent)),
-      home: const MyHomePage(),
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'SLUGarbage',
+        theme: ThemeData(
+            appBarTheme: AppBarTheme(backgroundColor: Colors.yellow.shade600),
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.orangeAccent)),
+        home: const MyHomePage(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class MyAppState extends ChangeNotifier {
+  var current = '';
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  void text(String stuff) {
+    current = stuff;
+    notifyListeners();
+  }
+
+  var names = <String>[];
+
+  void toggleNames() {
+    if (names.contains(current)) {
+      names.remove(current);
+    } else {
+      names.add(current);
+    }
+    notifyListeners();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('SLUGarbage')),
-      body: const TextInputWidget(),
-    );
-  }
-}
-
-class TextInputWidget extends StatefulWidget {
-  const TextInputWidget({super.key});
-
-  @override
-  State<TextInputWidget> createState() => _TextInputWidgetState();
-}
-
-class _TextInputWidgetState extends State<TextInputWidget> {
-  final textController = TextEditingController();
-
-  @override
-  void dispose() {
-    super.dispose();
-    textController.dispose();
-  }
-
-  void click() {}
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-          prefixIcon: const Icon(Icons.person),
-          labelText: "Enter a Name",
-          suffixIcon: IconButton(
-            icon: const Icon(Icons.send),
-            tooltip: "Submit",
-            onPressed: click,
-          )),
+      body: TextInputWidget(),
     );
   }
 }
---
+++ lib/text_input_widget.dart
@@ -0,0 +1,53 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:slugarbage/main.dart';
+
+class TextInputWidget extends StatelessWidget {
+  TextInputWidget({super.key});
+  final controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var name = appState.current;
+
+    IconData nameIcon;
+    String toggle = '';
+    if (appState.names.contains(name)) {
+      nameIcon = Icons.person_remove;
+      toggle = 'Remove';
+    } else {
+      nameIcon = Icons.person_add;
+      toggle = 'Add';
+    }
+
+    return Column(children: [
+      Container(
+        padding: const EdgeInsets.all(100.0),
+        child: TextField(
+          controller: controller,
+          decoration: InputDecoration(
+              prefixIcon: const Icon(Icons.person),
+              labelText: "Enter a Name",
+              suffixIcon: IconButton(
+                icon: const Icon(Icons.send),
+                tooltip: "Submit",
+                onPressed: () {
+                  appState.text(controller.text);
+                },
+              )),
+        ),
+      ),
+      ElevatedButton.icon(
+        onPressed: () {
+          appState.toggleNames();
+        },
+        icon: Icon(nameIcon),
+        label: Text(toggle),
+      ),
+      Text(appState.current),
+    ]);
+  }
+}
