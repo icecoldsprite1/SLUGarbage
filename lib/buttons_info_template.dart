cc455a2cb5dac5cc96e09789cf879c012b6311d6
Jeffrey <67610746+icecoldsprite1@users.noreply.github.com>
2024-02-25 03:44:33 -0800

    template files

---
+++ lib/buttons_info_template.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ButtonInfo extends StatelessWidget {
+  const ButtonInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/item_info_template.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ItemInfo extends StatelessWidget {
+  const ItemInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
